1eabd4e584cd4e24f908ce2f88668c03a72a829c
hzh <hzh@mail.imu.edu.cn>
2018-01-30 17:34:04 +0800

    init

---
+++ trees.py
@@ -0,0 +1,126 @@
+from math import log
+import operator
+
+
+def calShannonEnt(dataSet):
+    """
+    计算给定数据集的香农熵
+    :param dataSet:
+    :return:
+    """
+    numEntries = len(dataSet)
+    labelCounts = {}
+    for featVec in dataSet:
+        currentLabel = featVec[-1]
+        if currentLabel not in labelCounts.keys():
+            labelCounts[currentLabel] = 0
+        labelCounts[currentLabel] += 1
+    shannonEnt = 0.0
+    for key in labelCounts:
+        prob = float(labelCounts[key]) / numEntries
+        shannonEnt -= prob * log(prob, 2)
+    return shannonEnt
+
+
+def createDataset():
+    dataSet = [[1, 1, 'yes'],
+               [1, 1, 'yes'],
+               [1, 0, 'no'],
+               [0, 1, 'no'],
+               [0, 1, 'no']]
+    labels = ['no surfacing', 'flippers']
+    return dataSet, labels
+
+
+def splitDataSet(dataSet, axis, value):
+    """
+    按照给定特征划分数据集
+    :param dataSet:
+    :param axis:
+    :param value:
+    :return:
+    """
+    retDataSet = []
+    for featVec in dataSet:
+        if featVec[axis] == value:
+            reducedFeatVec = featVec[:axis]
+            reducedFeatVec.extend(featVec[axis + 1:])
+            retDataSet.append(reducedFeatVec)
+    return retDataSet
+
+
+def chooseBestFeatureToSplit(dataSet):
+    """
+    选择最好的数据集划分方式
+    :param dataSet:
+    :return:
+    """
+    numFeatures = len(dataSet[0]) - 1
+    # 信息熵
+    baseEntropy = calShannonEnt(dataSet)
+    bestInfoGain = 0.0
+    bestFeature = -1
+    for i in range(numFeatures):
+        featList = [example[i] for example in dataSet]
+        uniqueVals = set(featList)
+        # 条件熵
+        newEntropy = 0.0
+        for value in uniqueVals:
+            subDataSet = splitDataSet(dataSet, i, value)
+            prob = len(subDataSet) / float(len(dataSet))
+            newEntropy += prob * calShannonEnt(subDataSet)
+        # 信息增益 = 信息熵 - 条件熵
+        infoGain = baseEntropy - newEntropy
+        if infoGain > bestInfoGain:
+            bestInfoGain = infoGain
+            bestFeature = i
+    return bestFeature
+
+
+def majorityCnt(classList):
+    classCount = {}
+    for vote in classList:
+        if vote not in classCount.keys():
+            classCount[vote] = 0
+        classCount[vote] += 1
+    sortedClassCount = sorted(classCount.items(),
+                              key=operator.itemgetter(1),
+                              reverse=True)
+    return sortedClassCount[0][0]
+
+
+def createTree(dataSet, labels):
+    """
+    创建树
+    :param dataSet:
+    :param labels:
+    :return:
+    """
+    classList = [example[-1] for example in dataSet]
+    if classList.count(classList[0]) == len(classList):
+        return classList[0]
+    if len(dataSet[0]) == 1:
+        return majorityCnt(classList)
+    bestFeat = chooseBestFeatureToSplit(dataSet)
+    bestFeatLabel = labels[bestFeat]
+    myTree = {bestFeatLabel: {}}
+    del (labels[bestFeat])
+    featValues = [example[bestFeat] for example in dataSet]
+    uniqueVals = set(featValues)
+    for value in uniqueVals:
+        subLabels = labels[:]
+        myTree[bestFeatLabel][value] = createTree(
+            splitDataSet(dataSet, bestFeat, value), subLabels)
+    return myTree
+
+
+def main():
+    # myData, labels = createDataset()
+    # myTree = createTree(myData, labels)
+    # print(myTree)
+    myTree = {1: {}}
+    print(myTree)
+
+
+if __name__ == '__main__':
+    main()
